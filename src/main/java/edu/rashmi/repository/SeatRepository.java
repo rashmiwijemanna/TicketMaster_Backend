6cb4ead1cd89e4c016a2dc729b7333b8086f2ab6
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2026-01-20 13:28:24 +0530

    implement seat holding logic with pessimistic locking

--- src/main/java/edu/rashmi/repository/SeatRepository.java
+++ src/main/java/edu/rashmi/repository/SeatRepository.java
@@ -1,7 +1,16 @@
 package edu.rashmi.repository;
 
 import edu.rashmi.model.entity.Seat;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository< Seat, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Seat s WHERE s.id = :id")
+    Optional<Seat> findByWithLock(@Param("id") Long id);
 }
