b28a12cf88206858b95c757bc062e81ea77e2519
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2026-01-20 10:38:28 +0530

    implement feilds in AuditLog class

---
+++ src/main/java/edu/rashmi/model/entity/AuditLog.java
@@ -0,0 +1,27 @@
+package edu.rashmi.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Data
+@Builder
+public class AuditLog {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String actions;
+    private Long userId;
+    private String details;
+    private LocalDateTime timestamp;
+
+}
